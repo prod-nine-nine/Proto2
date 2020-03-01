dd33d62d13e61d346a139cddbb65c83b5fe7d39c
rhynz <rhynz@hotmail.co.nz>
2020-03-02 09:49:31 +1300

    project pushed

---
+++ Source/HoverDeathRace/HoverDeathRace.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HoverDeathRace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HoverDeathRace, "HoverDeathRace" );
